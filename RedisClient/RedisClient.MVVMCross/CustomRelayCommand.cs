7148f3f1864f2fa971dbb5550bd799c84b067cd9
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-10-04 10:45:43 +0300

    add commands

---
+++ RedisClient/RedisClient.MVVMCross/CustomRelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace RedisClient.MVVMCross
+{
+	public sealed class CustomRelayCommand : ICommand
+	{
+		private readonly Predicate<object> _canExecute;
+		private readonly Action<object> _execute;
+
+		public CustomRelayCommand(Predicate<object> canExecute, Action<object> execute)
+		{
+			_canExecute = canExecute;
+			_execute = execute;
+		}
+
+		public event EventHandler CanExecuteChanged
+		{
+			add => CommandManager.RequerySuggested += value;
+			remove => CommandManager.RequerySuggested -= value;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return _canExecute(parameter);
+		}
+
+		public void Execute(object parameter)
+		{
+			_execute(parameter);
+		}
+	}
+}
--- RedisClient/RedisClient.MVVMCross/ViewModel/ConnectionViewModel.cs
+++ RedisClient/RedisClient.MVVMCross/ViewModel/ConnectionViewModel.cs
@@ -1,18 +1,31 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+using System.Windows;
+using System.Windows.Input;
 
 namespace RedisClient.MVVMCross.ViewModel
 {
 	public sealed class ConnectionViewModel : MvxViewModel
 	{
+		#region Fields
 		private string? _ipAddress;
 		private int _port;
+		private bool _isConnectionOk;
+		#endregion
 
+		#region Ctor
 		public ConnectionViewModel()
 		{
 			IpAddress = "127.0.0.1";
 			Port = 7778;
-		}
+			IsConnectionOk = false;
 
+			TestConnectionCommand = new MvxCommand(TestConnection);
+			ConnectCommand = new CustomRelayCommand((o) => IsConnectionOk == true, Connect);
+		}
+		#endregion
+
+		#region Properties
 		public string? IpAddress
 		{
 			get => _ipAddress;
@@ -24,5 +37,28 @@ namespace RedisClient.MVVMCross.ViewModel
 			get => _port;
 			set => SetProperty(ref _port, value);
 		}
+
+		public bool IsConnectionOk
+		{
+			get => _isConnectionOk;
+			set => SetProperty(ref _isConnectionOk, value);
+		}
+
+		public IMvxCommand TestConnectionCommand { get; }
+		public ICommand ConnectCommand { get; }
+		#endregion
+
+
+		#region Event Handlers
+		private void Connect(object o)
+		{
+			IsConnectionOk = false;
+		}
+
+		private void TestConnection()
+		{
+			IsConnectionOk = true;
+		}
+		#endregion
 	}
 }
