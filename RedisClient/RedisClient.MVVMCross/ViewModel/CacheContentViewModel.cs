449f0df3065ce6ae0c16c29c730eef6d6d52b10c
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-10-05 17:41:48 +0300

    add delete

--- RedisClient/RedisClient.MVVMCross/ViewModel/CacheContentViewModel.cs
+++ RedisClient/RedisClient.MVVMCross/ViewModel/CacheContentViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using MvvmCross.Commands;
 using MvvmCross.ViewModels;
 using RedisClient.Core;
+using StackExchange.Redis;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,34 +10,51 @@ namespace RedisClient.MVVMCross.ViewModel
 	public sealed class CacheContentViewModel : MvxViewModel
 	{
 		#region Fields
-		private ICacheReader _cacheReader;
-		private MvxObservableCollection<KeyValuePair<string, string>> _cacheContent;
+		private readonly ICacheReader _cacheReader;
+		private readonly ICacheWriter _cacheWriter;
+		private MvxObservableCollection<KeyValuePair<RedisKey, RedisValue>> _cacheContent;
+		private KeyValuePair<RedisKey, RedisValue> _selectedCacheContent;
 		#endregion
 
 		#region Ctor
-		public CacheContentViewModel(IRedisServerConnector redisServerConnector, ICacheReader cacheReader)
+		public CacheContentViewModel(IRedisServerConnector redisServerConnector, ICacheReader cacheReader, ICacheWriter cacheWriter)
 		{
 			_cacheReader = cacheReader;
-			CacheContent = new MvxObservableCollection<KeyValuePair<string, string>>();
+			_cacheWriter = cacheWriter;
+			CacheContent = new MvxObservableCollection<KeyValuePair<RedisKey, RedisValue>>();
 			RefreshCommand = new MvxCommand(async () => await Refresh());
+			DeleteCommand = new MvxCommand(async () => await Delete());
 		}
 		#endregion
 
 		public IMvxCommand RefreshCommand { get; }
+		public IMvxCommand DeleteCommand { get; }
 
 		private async Task Refresh()
 		{
 			var res1 = await _cacheReader.GetAllKeysAsync();
-			IList<KeyValuePair<string, string>> cacheItems = new List<KeyValuePair<string, string>>();
+			IList<KeyValuePair<RedisKey, RedisValue>> cacheItems = new List<KeyValuePair<RedisKey, RedisValue>>();
 			foreach (var item in res1)
 			{
 				var v = await _cacheReader.GetAsync(item);
-				cacheItems.Add(new KeyValuePair<string, string>(item.ToString(), v.ToString()));
+				cacheItems.Add(new KeyValuePair<RedisKey, RedisValue>(item, v));
 			}
-			CacheContent = new MvxObservableCollection<KeyValuePair<string, string>>(cacheItems);
+			CacheContent = new MvxObservableCollection<KeyValuePair<RedisKey, RedisValue>>(cacheItems);
 		}
 
-		public MvxObservableCollection<KeyValuePair<string, string>> CacheContent
+		private async Task Delete()
+		{
+			await _cacheWriter.RemoveAsync(SelectedCacheContent.Key);
+			await Refresh();
+		}
+
+		public KeyValuePair<RedisKey, RedisValue> SelectedCacheContent
+		{
+			get => _selectedCacheContent;
+			set => SetProperty(ref _selectedCacheContent, value);
+		}
+
+		public MvxObservableCollection<KeyValuePair<RedisKey, RedisValue>> CacheContent
 		{
 			get => _cacheContent;
 			set => SetProperty(ref _cacheContent, value);
